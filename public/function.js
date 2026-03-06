c5c6d3058f5a13689123a63abfffe49885191919
Andrizzz1 <andreimandapat09@gmail.com>
2026-03-07 01:57:26 +0800

    Added a chatbot design but still not functional

--- public/function.js
+++ public/function.js
@@ -1,8 +1,36 @@
 const HamMenuBtn = document.querySelector("#HamMenu")
 const MobileNav = document.getElementById("MobileNav")
+const Chatbot = document.getElementById("Chatbot")
+const OpenChatbot = document.getElementById("Openchatbot")
+const ChatbotText = document.getElementById("ChatbotText")
+const User = document.getElementById("UserMessage")
+const SendMess = document.getElementById("Sendbtn")
+
+ChatbotText.addEventListener('click',()=>{
+    OpenChatbot.classList.toggle("hidden")
+    if (ChatbotText.textContent == "Open Chatbot"){
+        ChatbotText.textContent = "Close Chatbot"
+
+    }else{
+        ChatbotText.textContent = "Open Chatbot"
+    }
+
+})
 
 HamMenuBtn.addEventListener("click",()=>{
     MobileNav.classList.toggle("hidden")
 })
+
+UserMsgList = []
+SendMess.addEventListener('click',()=>{
+    const msg = User.value.trim();
+    if (msg !== ""){
+        UserMsgList.push(msg)
+    }else{
+        alert("Empty input")
+    }
+    User.value = '';
+})
+
 
 
